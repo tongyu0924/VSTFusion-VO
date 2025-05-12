4da7e9d592731b21d5a635ca1a6d2094fa32ad97
tongyu <winnie920924@gmail.com>
2025-05-13 04:05:01 +0800

    Update build.py

--- timesformer/datasets/build.py
+++ timesformer/datasets/build.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from fvcore.common.registry import Registry
 
