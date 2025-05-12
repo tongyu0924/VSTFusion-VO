1f8df87f2178a5b851ebd3920fd291873f0ba087
tongyu <winnie920924@gmail.com>
2025-05-13 04:39:24 +0800

    Update weight_init_helper.py

--- timesformer/utils/weight_init_helper.py
+++ timesformer/utils/weight_init_helper.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved. 
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 """Utility function for weight initialization"""
 
