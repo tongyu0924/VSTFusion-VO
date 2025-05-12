1dbeead151c3568c64c6fb58e9da797dec3a97d3
tongyu <winnie920924@gmail.com>
2025-05-13 04:04:44 +0800

    Update __init__.py

--- timesformer/datasets/__init__.py
+++ timesformer/datasets/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from .build import DATASET_REGISTRY, build_dataset  # noqa
 from .kinetics import Kinetics  # noqa
