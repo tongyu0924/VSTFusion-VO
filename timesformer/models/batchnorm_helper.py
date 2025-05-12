6c259dcb562e451b963c62a7fde8b01152141b7d
tongyu <winnie920924@gmail.com>
2025-05-13 04:09:40 +0800

    Update batchnorm_helper.py

--- timesformer/models/batchnorm_helper.py
+++ timesformer/models/batchnorm_helper.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 """BatchNorm (BN) utility functions and custom batch-size BN implementations"""
 
