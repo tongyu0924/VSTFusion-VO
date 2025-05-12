88608ba0097c6d9e4e00f204e11b6e3480003adb
tongyu <winnie920924@gmail.com>
2025-05-13 04:09:16 +0800

    Update __init__.py

--- timesformer/models/__init__.py
+++ timesformer/models/__init__.py
@@ -1,4 +1,4 @@
-# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved
+# Copyright (c) Facebook, Inc. and its affiliates. All Rights Reserved.
 
 from .build import MODEL_REGISTRY, build_model  # noqa
 from .custom_video_model_builder import *  # noqa
